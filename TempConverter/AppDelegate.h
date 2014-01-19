8d66e9cdca3c39f278c58e45cd328e7d32146c91
Fei Xie <feix25@gmail.com>
2014-01-18 20:48:08 -0800

    Initial Commit

---
+++ TempConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TempConverter
+//
+//  Created by fxie on 1/18/14.
+//  Copyright (c) 2014 fxie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
